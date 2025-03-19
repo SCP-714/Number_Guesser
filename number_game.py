8c50d27843711c599b26796df924f9cc039871a6
SCP-714 <113054208+SCP-714@users.noreply.github.com>
2025-03-18 21:30:24 -0500

    added if else

--- number_game.py
+++ number_game.py
@@ -5,5 +5,20 @@ start = input("Welcome traveler, would you like to continue to a journey of ridd
 if start.lower() != "yes":
     quit()
 
-random_number = random.randint(0, 100)
-print(random_number)
+top_of_range = input("Traveler please enter a number: ")
+
+ 
+
+random_number = random.randint(0, top_of_range)
+
+while True:
+    user_guess = input("Make a Guess Traveler: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+
+    if user_guess <= 0:
+        print("Please Provide A Number Greater Than 0 Next Time: ")
+        quit()
+    else:
+        print("Please Provide A Number Next Time")
+        quit()
